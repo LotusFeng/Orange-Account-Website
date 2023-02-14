3a486d4387b7253f7cefdca3a233b06305eca72d
czf <505487452@qq.com>
2023-02-14 15:21:52 +0800

    update

---
+++ assets/TagCreate.6cb34eed.js
@@ -0,0 +1,1 @@
+import{B as t,M as a}from"./index.46d6784b.js";import{T as e}from"./TagForm.dd22c765.js";import{bE as n,bF as o}from"./vendor.9e53a908.js";import"./vant.226ba7de.js";const i=n({setup:(r,l)=>()=>o(a,null,{title:()=>"\u65B0\u5EFA\u6807\u7B7E",icon:()=>o(t,null,null),default:()=>o(e,null,null)})});export{i as TagCreate,i as default};
